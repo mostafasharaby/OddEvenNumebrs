bd9e6b2f5a1568908b8106c0ed43a64aa62ea94a
mostafasharaby <138678777+mostafasharaby@users.noreply.github.com>
2024-10-20 21:59:35 +0300

    second day

---
+++ Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+
+
+class Program
+{
+    static void Main()
+    {
+
+
+        // Use extension method to get even numbers
+        List<int> evenNumbers = numbers.FilterNumbers(IsEven);
+        Console.WriteLine("Even Numbers: " + string.Join(", ", evenNumbers));
+
+        // Use extension method to get odd numbers
+        List<int> oddNumbers = numbers.FilterNumbers(IsOdd);
+        Console.WriteLine("Odd Numbers: " + string.Join(", ", oddNumbers));
+        // Use extension method to get even numbers
+        List<int> evenNumbers = numbers.FilterNumbers(IsEven);
+        Console.WriteLine("Even Numbers: " + string.Join(", ", evenNumbers));
+
+        // Use extension method to get odd numbers
+        List<int> oddNumbers = numbers.FilterNumbers(IsOdd);
+        Console.WriteLine("Odd Numbers: " + string.Join(", ", oddNumbers));
+    }
+    // Use extension method to get even numbers
+    List<int> evenNumbers = numbers.FilterNumbers(IsEven);
+    Console.WriteLine("Even Numbers: " + string.Join(", ", evenNumbers));
+
+        // Use extension method to get odd numbers
+        List<int> oddNumbers = numbers.FilterNumbers(IsOdd);
+    Console.WriteLine("Odd Numbers: " + string.Join(", ", oddNumbers));
+    }
+// Use extension method to get even numbers
+List<int> evenNumbers = numbers.FilterNumbers(IsEven);
+Console.WriteLine("Even Numbers: " + string.Join(", ", evenNumbers));
+
+// Use extension method to get odd numbers
+List<int> oddNumbers = numbers.FilterNumbers(IsOdd);
+Console.WriteLine("Odd Numbers: " + string.Join(", ", oddNumbers));
+    }
+}
+
+    // Method to check if a number is even
+   
+    }
+}
